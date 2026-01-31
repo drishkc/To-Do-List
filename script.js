507d6336046f33d33d5ef9913008032752dfa3b4
drishkc <kcdrish38@gmail.com>
2026-01-31 22:20:29 +0545

    html and css done

---
+++ script.js
@@ -0,0 +1,13 @@
+// const delHvr = document.querySelectorAll(".del")
+// const cont = document.querySelectorAll(".contOne")
+// cont.forEach(el => {
+//     el.addEventListener("mouseover", () => {
+//     delHvr.style.display = "inline-flex"
+//    })
+// })
+// cont.forEach(el => {
+//     el.addEventListener("mouseout", () => {
+//     delHvr.style.display = "none"
+//    })
+// })
+const 
