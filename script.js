352cf1279859da0f5a5e6e476c88b0f609d82271
drishkc <kcdrish38@gmail.com>
2026-02-06 10:44:41 +0545

    remodelled li

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-// const delHvr = document.querySelectorAll(".del")
-// const cont = document.querySelectorAll(".contOne")
-// cont.forEach(el => {
-//     el.addEventListener("mouseover", () => {
-//     delHvr.style.display = "inline-flex"
-//    })
-// })
-// cont.forEach(el => {
-//     el.addEventListener("mouseout", () => {
-//     delHvr.style.display = "none"
-//    })
-// })
-const 
+
+const blur = document.querySelector(".blur")
+const lists = document.querySelector(".lists")
+const submit = document.querySelector(".submitBtn")
+
+submit.addEventListener('click', () => {
+    let usrInpPara = document.querySelector(".msgField")
+    const del = document.createElement("span")
+    del.innerText = "x"
+    let li = document.createElement("li")
+    li.classList.add ("contOne")
+    li.innerText = usrInpPara.value
+    lists.appendChild(li)
+    li.appendChild(del)
+    usrInpPara.value = ""
+});
+lists.addEventListener('click', (e)=> {
+    const item = e.target.closest(".contOne")
+    if(!item) return
+
+    item.classList.toggle("checked")
+    item.classList.toggle("liChecked")
+})
